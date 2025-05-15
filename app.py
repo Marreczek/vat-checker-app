2f0efc6ff2a3a27a5074af947928dd7f875f0a4d
Marreczek <izdebska.mart@gmail.com>
2025-05-15 21:19:18 +0200

    threading

--- app.py
+++ app.py
@@ -127,4 +127,5 @@ def pobierz_wynik(task_id):
     return "Plik niedostępny."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
